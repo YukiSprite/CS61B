bb7203e342ef4a427b733f3fb7aef3ed2cb8cb52
Yuki <laohouxing@outlook.com>
2024-08-21 19:55:18 +0800

    reduce memory leak

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,31 +1,46 @@
 public class ArrayDeque<T>  {
     private int size;
     private T[] items;
+    private int head;
+    private int tail;
+    private int capacity;
     public ArrayDeque() {
         size = 0;
         items = (T []) new Object[8];
+        head = 0;
+        tail = 0;
+        capacity = 8;
     }
-    private void resize( int capacity ){
-        T[] tmpItems = (T []) new Object[capacity];
-        System.arraycopy(items, 0, tmpItems, 0, items.length);
-        items = tmpItems;
+    private void resize(int newCapacity) {
+        T[] newArray = (T[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = items[(head + i) % capacity];
+        }
+        items = newArray;
+        head = 0;
+        tail = size;
+        capacity = newCapacity;
     }
     public void addFirst( T item ){
-        if( size == items.length ){
+        if( size >= (items.length-1) ){
             resize(size * 2);
         }
-        T[] tmpItems = (T[]) new Object[size];
-        System.arraycopy(items,0,tmpItems,1,items.length);
-        tmpItems[0] = item;
-        items = tmpItems;
+        head--;
+        if(head<0){
+            head = capacity-1;
+        }
+        items[head] = item;
+        size++;
     }
     public void addLast(T item){
-        if(size==items.length){
+        if( size >= (items.length-1) ){
             resize(size*2);
         }
-        T[] tmpItems = (T[]) new Object[size];
-        System.arraycopy(items,0,tmpItems,0,items.length);
-        tmpItems[items.length-1] = item;
+        tail++;
+        if(tail>(capacity-1)){
+            tail = 0;
+        }
+        items[tail] = item;
     }
     public boolean isEmpty(){
         return size==0;
@@ -39,27 +54,40 @@ public class ArrayDeque<T>  {
         }
     }
     public T removeFirst(){
-        T[] tmpItems = (T []) new Object[size];
-        T item = items[0];
-        System.arraycopy(items,1,tmpItems,0,size-1);
-        items = tmpItems;
-        if(items.length<(size/4)){
-            resize(size/2);
+        if(size==0){
+            return null;
         }
+        T item = items[head];
+        items[head] = null;
+        head++;
+        if( head > (capacity-1)){
+            head = 0;
+        }
+        if (size > 0 && size == capacity / 4) {
+            resize(capacity / 2);
+        }
+        size--;
         return item;
+
     }
     public T removeLast(){
-        T[] tmpItems = (T []) new Object[size];
-        T item = items[size-1];
-        System.arraycopy(items,0,tmpItems,0,size-1);
-        items = tmpItems;
-        if(items.length<(size/4)){
-            resize(size/2);
+        if(size==0){
+            return null;
         }
+        T item = items[tail];
+        items[tail] = null;
+        tail--;
+        if(tail<0){
+            tail = capacity-1;
+        }
+        if (size > 0 && size == capacity / 4) {
+            resize(capacity / 2);
+        }
+        size--;
         return item;
     }
     public T get(int index){
-        return items[index];
+        return items[(head+index)%capacity];
     }
 
 }
