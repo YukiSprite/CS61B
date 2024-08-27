9627d064dacdd58977fa2f5d7048509955cfc50a
Yuki <laohouxing@outlook.com>
2024-08-27 10:42:12 +0800

    didn't do optional part

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,107 @@
+// TODO: Make sure to make this class a part of the synthesizer package:done
+ package synthesizer;
+import org.hamcrest.internal.ArrayIterator;
+
+import java.util.Iterator;
+
+//TODO: Make sure to make this class and all of its methods public:done
+//TODO: Make sure to make this class extend AbstractBoundedQueue<t>:done
+public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        this.capacity = capacity;
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        // TODO: Create new array with capacity elements.
+        //       first, last, and fillCount should all be set to 0.
+        //       this.capacity should be set appropriately. Note that the local variable
+        //       here shadows the field we inherit from AbstractBoundedQueue, so
+        //       you'll need to use this.capacity to set the capacity.
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public void enqueue(T x) {
+        if (isFull()){
+            throw new RuntimeException("Ring buffer overflow");
+        }
+            rb[last] = x;
+            last = (last + 1) % capacity;
+            fillCount++;
+        // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public T dequeue() {
+        if (isEmpty()){
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T tmp = rb[first];
+        rb[first] = null;
+        first++;
+        if(first == capacity) {
+            first = 0;
+        }
+        fillCount--;
+        return tmp;
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    @Override
+    public T peek() {
+        return rb[first];
+        // TODO: Return the first item. None of your instance variables should change.
+    }
+    @Override
+    public int fillCount() {
+        return fillCount;
+    }
+    @Override
+    public int capacity(){
+        return capacity;
+    }
+    // TODO: When you get to part 5, implement the needed code to support iteration.
+    @Override
+    public Iterator<T> iterator() {
+        return new myIterator();
+    }
+   private class  myIterator implements Iterator<T>  {
+        private int pos;
+        public myIterator() {
+            pos = 0;
+        }
+        @Override
+        public boolean hasNext() {
+            return pos < capacity;
+        }
+        @Override
+        public T next() {
+            T returnValue = rb[pos];
+            pos++;
+            return returnValue;
+        }
+    }
+}
--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -1,5 +1,5 @@
 // TODO: Make sure to make this class a part of the synthesizer package
-//package <package name>;
+package synthesizer;
 
 //Make sure this class is public
 public class GuitarString {
@@ -14,6 +14,10 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
+        buffer = new ArrayRingBuffer<Double>((int)Math.round(SR/frequency));
+        while(!buffer.isFull()) {
+            buffer.enqueue((double)0);
+        }
         // TODO: Create a buffer with capacity = SR / frequency. You'll need to
         //       cast the result of this divsion operation into an int. For better
         //       accuracy, use the Math.round() function before casting.
@@ -23,6 +27,12 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
+        while(!buffer.isEmpty()) {
+            buffer.dequeue();
+        }
+        while(!buffer.isFull()) {
+            buffer.enqueue((Math.random() - 0.5));
+        }
         // TODO: Dequeue everything in the buffer, and replace it with random numbers
         //       between -0.5 and 0.5. You can get such a number by using:
         //       double r = Math.random() - 0.5;
@@ -34,6 +44,9 @@ public class GuitarString {
      * the Karplus-Strong algorithm. 
      */
     public void tic() {
+        double firstVal = buffer.dequeue();
+        double secondVal = buffer.peek();
+        buffer.enqueue(DECAY*(firstVal + secondVal)/2);
         // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       Do not call StdAudio.play().
@@ -42,6 +55,6 @@ public class GuitarString {
     /* Return the double at the front of the buffer. */
     public double sample() {
         // TODO: Return the correct thing.
-        return 0;
+        return buffer.peek();
     }
 }
