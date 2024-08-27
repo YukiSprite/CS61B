fb1c7f72a3b54db025fc3df37c0d276ba559c32a
Yuki <laohouxing@outlook.com>
2024-08-27 11:04:00 +0800

    didn't do optional part, improve exception

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -9,4 +9,7 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     public int fillCount(){
         return fillCount;
     }
+    public int capacity() {
+        return capacity;
+    }
 }
